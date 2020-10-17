32afc3a23f163e65b3a8747468289d145aa13ed4
RyosukeKamimura <come1234560@gmail.com>
2020-10-17 14:29:31 +0900

    Add Landing Page🎨

--- vueproject/src/views/Landing.vue
+++ vueproject/src/views/Landing.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    ランディングページ
-  </div>
+  <v-container fluid fill-height>
+    <v-row
+      class="blue lighten-4"
+      style="height: 450px;"
+      justify="center"
+      align-content="center">
+        <v-img
+        contain
+        max-height="295"
+        max-width="500"
+        src="../assets/top-image.png"
+        ></v-img>
+      <v-col>
+        <h1 class="mb-10">
+          時間の達人
+        </h1>
+        <v-btn
+          depressed
+          elevation="15"
+          raised
+          rounded
+          x-large
+        >時間の達人になる</v-btn>
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
