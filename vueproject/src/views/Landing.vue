6f6ced1a73a35f32334c3183ff3459b620a745bf
RyosukeKamimura <come1234560@gmail.com>
2020-10-17 19:25:33 +0900

    Completed User Page 🎉

--- vueproject/src/views/Landing.vue
+++ vueproject/src/views/Landing.vue
@@ -1,29 +1,38 @@
 <template>
-  <v-container fluid fill-height>
-    <v-row
-      class="blue lighten-4"
-      style="height: 450px;"
-      justify="center"
-      align-content="center">
+  <v-app>
+    <v-container fluid fill-height class="purple darken-4">
+      <v-row
+        class="purple darken-4"
+        style="height: 450px"
+        justify="center"
+        align-content="center"
+      >
         <v-img
-        contain
-        max-height="295"
-        max-width="500"
-        src="../assets/top-image.png"
+          contain
+          max-height="600"
+          max-width="600"
+          src="../assets/top-image.png"
         ></v-img>
-      <v-col>
-        <h1 class="mb-10">
-          時間の達人
-        </h1>
-        <v-btn
-          depressed
-          elevation="15"
-          raised
-          rounded
-          x-large
-        >時間の達人になる</v-btn>
-      </v-col>
-
-    </v-row>
-  </v-container>
+        <v-col>
+          <h1 class="mb-10 font-weight-black white--text">自分を追い込むたった一つの方法</h1>
+          <p class="mb-10 font-weight-black white--text">当サービスは、秘密を書き込み決められた時間にタスクが終わらな買った場合、強制的にTwitterに書き込まれるクレイジーでストイックなサービスです。</p>
+          <v-btn class="cyan accent-3 black--text font-weight-regular" depressed elevation="15" raised rounded x-large
+            >時間の達人になる</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
+
+<script>
+export default {
+  theme: {
+    themes: {
+      light: {
+        background: "f5f5f5",
+      },
+    },
+  },
+};
+</script>
--- vueproject/src/views/User.vue
+++ vueproject/src/views/User.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    ユーザー個別ページ
-  </div>
+  <v-form>
+    <v-container fluid fill-height align="center">
+      <v-row align="center">
+        <v-col cols="12" sm="6">
+          <v-text-field
+            class="mb-10"
+            v-model="secret_mess"
+            :rules="rules"
+            counter="50"
+            hint="絶対誰にも言わないから"
+            label="とっておきの秘密を教えて！"
+          ></v-text-field>
+
+          <v-btn depressed color="primary">これに決定！</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      secret_mess: "",
+      rules: [v => v.length <= 50 || 'Max 50 characters'],
+    }
+  },
+};
+</script>
