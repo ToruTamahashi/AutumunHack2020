8d5de78ccb6742aa4306cdc03be63b18a3752940
ToruTamahashi <0ys25x4t725473s@gmail.com>
2020-10-19 14:10:35 +0900

    ツイッター認証 ユーザ登録 ユーザ情報取得実装

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -2,19 +2,23 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
-
+import  tweepy
 from .model.models import UserEntity
 from .model.models import TaskEntity
 from .model.models import UserService
 from .model.models import TaskService
 from .develop.self_req import get_req
 from .develop.self_req import post_req
-from .develop.auto_tweet import TwitterAPI
+from .develop.self_req import oauth_test
+from .develop.auto_tweet import TwAPI
+from .develop.auto_tweet import OAuthTwitter
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app, origin=['localhost','hackwebapps.net','autumn.hackwebapps.net'],allow_headers=['Content-Type','Authorization'],
      methods=['GET', 'POST', 'PUT', 'DELETE'])
-twitter = TwitterAPI()
+
+
+
 
 @app.route('/')
 def hello_world():
@@ -64,29 +68,65 @@ def id_world(id):
 @app.route('/req')
 def self_request():
     res = post_req()
-    #res = get_req()
+    res = get_req()
+    res = oauth_test()
     return res.text
+
+@app.route('/user_info', methods=['POST'])
+def user_info():
+    """
+    ユーザ情報を取得します。（ユーザ名,twitter_id）
+    :param　json ex) {'access_token_key':'*****','access_token_secret':'*****'}
+    :return: json ex) {'name':'***','twitter_id':'****'}
+    """
+    access_token_dict = request.get_json(force=True)
+    tw = TwAPI()
+    user = tw.get_user_info(access_token_dict['access_token_key'],access_token_dict['access_token_secret'])
+    user_info = {'name':user.name,'twitter_id':user.screen_name}
+    return jsonify(user_info)
+
 
 @app.route('/login', methods=['GET'])
 def login():
     """
     Twitter認証画面にリダイレクトさせるurlを返します
-    :return: String
+    :return: json
+            ex) {'url','https://****'}
     """
-    redirect_url = twitter.get_redirect_url()
-    return redirect_url
+    ot = OAuthTwitter()
+    redirect_url = ot.get_redirect_url()
+    print(redirect_url)
+    #redirect_url = twitter.get_redirect_url()
+    return jsonify({'url':redirect_url})
 
-@app.route('/get_task', methods=['POST'])
-def get_task():
+@app.route('/get_access_token', methods=['POST'])
+def get_access_token():
     """
-    oauth_verifierを受け取ってユーザ認証完了したらタスク一覧を返す
+    認証後リダイレクト先のurlに付与されているパラメータを受け取りアクセストークンを返す
+    :param json
+            ex)  {'oauth_token':'*****','oauth_verifier':'*****'}
     :return:json
+            ex)  {'access_token_key':'*****', 'access_token_secret':'******'}
     """
-    json_dict = request.get_json(force=True)
-    print(json_dict['oauth_verifier'])
-    #twitter.post_verifier(json_dict['oauth_verifier'])
+    oauth_dict = request.get_json(force=True)
+    ot = OAuthTwitter()
+    access_token_dict = ot.get_access_token(oauth_dict)
+    if access_token_dict != "error":
+        print("AccessToken")
+        print(access_token_dict)
+        #tw = TwitterAPI(access_token_dict['access_token_key'],access_token_dict['access_token_secret'])
+        #print()
+        return jsonify(access_token_dict)
+    else:
+        return "error"
+
+@app.route('/post_tweet', methods=['POST'])
+def get_task():
     #twitter.post_tweet()
-    return "a"
+    return "b"
+
+
+
 
 def main():
     app.debug = True
