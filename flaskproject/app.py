dba103ab116ce299e07bf7222e0c222154041761
ToruTamahashi <0ys25x4t725473s@gmail.com>
2020-10-22 01:06:53 +0900

    微修正

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
-import  tweepy
+import tweepy
 from .model.models import UserEntity
 from .model.models import TaskEntity
 from .model.models import UserService
